3bc2b7a9a24916591da665076c9e71ba4c2ea28b
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-10-20 01:24:06 -0700

    stable build version number / dev mode timestamp for version / add version ass url param for assets to prevent cache

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"fmt"
+	"time"
+	"flag"
+	"strconv"
 )
 
 type Film struct {
@@ -16,8 +19,16 @@ type Film struct {
 func main() {
 	fmt.Println("Starting server on http://localhost:42069")
 
-	rootHandler := func(w http.ResponseWriter, r *http.Request) {
+	dev := flag.String("dev", "false", "a bool")
+	flag.Parse()
+	version := "1.0.0"
+	
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
 		log.Println("URL Requested: ", r.URL.Path)
+		if (*dev == "true") {
+			 version = strconv.FormatInt(time.Now().Unix(), 10)
+		} 
 
 		if (r.URL.Path != "/") {
 			tmpl := template.Must(template.ParseFiles("templates/404.html"))
@@ -27,27 +38,11 @@ func main() {
 		}
 
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
-		
-		ctx := map[string][]Film{
-			"Films": {
-				{
-					Title: "The Shawshank Redemption",
-					Director: "Frank Darabont",
-					Year: 1994,
-					Actor: "Tim Robbins",
-				},
-				{
-					Title: "The Godfather",
-					Director: "Francis Ford Coppola",
-					Year: 1972,
-					Actor: "Marlon Brando",
-				},
-				{
-					Title: "The Dark Knight",
-					Director: "Christopher Nolan",
-					Year: 2008,
-					Actor: "Christian Bale",
-				},
+	
+		data := map[string]interface{
+		}{
+			"version":  version,
+			"Films": []Film {
 				{
 					Title: "The Shawshank Redemption",
 					Director: "Frank Darabont",
@@ -69,10 +64,10 @@ func main() {
 			},	
 		}
 
-		tmpl.Execute(w, ctx)
+		tmpl.Execute(w, data)
 	}
 
-	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/", handler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Fatal(http.ListenAndServe(":42069", nil))
 }
