fd148569f0b225132c90a6fbe8010660952087f8
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-10-21 01:08:19 -0700

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"time"
 	"strconv"
+	// "database/sql"
 )
 
 type Film struct {
@@ -21,11 +22,11 @@ func main() {
 	fmt.Println("Starting server on http://localhost:42069")
 	versionHash := version
 		
-	handler := func(w http.ResponseWriter, r *http.Request) {
+	requestHandler := func(w http.ResponseWriter, r *http.Request) {
 		log.Println("URL Requested: ", r.URL.Path)
 		if (version == "development") {
 			 versionHash = strconv.FormatInt(time.Now().UnixNano(), 10)
-		} 
+		}
 
 		if (r.URL.Path != "/") {
 			tmpl := template.Must(template.ParseFiles("templates/404.html"))
@@ -38,32 +39,36 @@ func main() {
 		data := map[string]interface{
 		}{
 			"Version":  versionHash,
-			"Films": []Film {
-				{
-					Title: "The Shawshank Redemption",
-					Director: "Frank Darabont",
-					Year: 1994,
-					Actor: "Tim Robbins",
-				},
-				{
-					Title: "The Godfather",
-					Director: "Francis Ford Coppola",
-					Year: 1972,
-					Actor: "Marlon Brando",
-				},
-				{
-					Title: "The Dark Knight",
-					Director: "Christopher Nolan",
-					Year: 2008,
-					Actor: "Christian Bale",
-				},
-			},	
+			"Films": getFilms(),	
 		}
 
 		tmpl.Execute(w, data)
 	}
 
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", requestHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Fatal(http.ListenAndServe(":42069", nil))
+}
+
+func getFilms() []Film {
+	return []Film {
+		{
+			Title: "The Shawshank Redemption",
+			Director: "Frank Darabont",
+			Year: 1994,
+			Actor: "Tim Robbins",
+		},
+		{
+			Title: "The Godfather",
+			Director: "Francis Ford Coppola",
+			Year: 1972,
+			Actor: "Marlon Brando",
+		},
+		{
+			Title: "The Dark Knight",
+			Director: "Christopher Nolan",
+			Year: 2008,
+			Actor: "Christian Bale",
+		},
+	}
 }
