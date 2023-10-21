8de50c3d4c48e3ee598900db2d7770bda8a71dfb
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-10-21 08:24:58 +0000

    trigger build on git pull

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 	"strconv"
 	// "database/sql"
+	"os/exec"
 )
 
 type Film struct {
@@ -25,7 +26,36 @@ func main() {
 	requestHandler := func(w http.ResponseWriter, r *http.Request) {
 		log.Println("URL Requested: ", r.URL.Path)
 		if (version == "development") {
-			 versionHash = strconv.FormatInt(time.Now().UnixNano(), 10)
+			versionHash = strconv.FormatInt(time.Now().UnixNano(), 10)
+		}
+		
+		if (r.URL.Path == "/_webhook/git-pull") {
+			// secret: wqaeKr65XJZiSL
+			// create a new *Cmd instance
+			// here we pass the command as the first argument and the arguments to pass to the command as the
+			// remaining arguments in the function
+			cmd := exec.Command("git", "pull")
+
+			// The `Output` method executes the command and
+			// collects the output, returning its value
+			out, err := cmd.Output()
+			if err != nil {
+			// if there was any error, print it here
+			fmt.Println("could not run command 'git pull': ", err)
+			}
+			// otherwise, print the output from running the command
+			fmt.Println("Output: ", string(out))
+	
+			cmd2 := exec.Command("npm", "run", "build")
+			out2, err2 := cmd2.Output()
+			if err2 != nil {
+			// if there was any error, print it here
+			fmt.Println("could not run command 'npm run build': ", err)
+			}
+			// otherwise, print the output from running the command
+			fmt.Println("Output: ", string(out2))
+
+			return
 		}
 
 		if (r.URL.Path != "/") {
