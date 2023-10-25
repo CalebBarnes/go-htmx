954463d7c3f39dd4d568067a64a61454989c4f6d
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-10-25 09:11:53 +0000

    getting page data and blocks directly from directus database

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.20
 
-require github.com/jmoiron/sqlx v1.3.5 // indirect
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"html/template"
 	"log"
@@ -9,6 +11,7 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 type Seo struct {
@@ -26,33 +29,37 @@ func main() {
 		log.Println("URL Requested: ", r.URL.Path)
 		if (version == "development") {
 			versionHash = strconv.FormatInt(time.Now().UnixNano(), 10)
+		}	
+		
+		pageData, err := getPageData(r.URL.Path)
+		
+		if err != nil {
+			tmpl := template.Must(template.ParseFiles("templates/404.go.html"))
+			template.Must(tmpl.ParseGlob("components/*.go.html"))
+			data := map[string]interface{}{
+				"Seo": Seo {
+					Title:"404 - Page not found",
+					Description: "You've hit a dead end...",
+				},
+			}
+			tmpl.Execute(w, data)
+			
+		} else {
+			log.Println("pageDataTest: ", pageData)
+			data := map[string]interface{
+				}{
+					"Version":  versionHash,
+					"Seo": Seo{
+						Title: pageData.Title,
+						Description: "This is the SEO description",
+					},
+					"Data": pageData,
+				}
+		
+				tmpl := template.Must(template.ParseFiles("templates/index.go.html"))
+				template.Must(tmpl.ParseGlob("components/*.go.html"))
+				tmpl.Execute(w, data)
 		}
-		
-		if (r.URL.Path != "/") {
-			http.NotFound(w, r)
-			return
-		}
-		
-		
-		pageDataTest := getPageFromDBByUrl(r.URL.Path)
-		log.Println("pageDataTest: ", pageDataTest)
-		
-		pageData := getPageData(r.URL.Path)
-
-		data := map[string]interface{
-		}{
-			"Version":  versionHash,
-			"Seo": Seo{
-				Title: pageData.Title,
-				Description: "This is the SEO description",
-			},
-			"Data": pageData,
-		}
-
-		tmpl := template.Must(template.ParseFiles("templates/index.go.html"))
-		template.Must(tmpl.ParseGlob("components/*.go.html"))
-
-		tmpl.Execute(w, data)
 	}
 
 	http.HandleFunc("/", requestHandler)
@@ -61,26 +68,29 @@ func main() {
 }
 
 
-type Blocker interface{}
-
 type Page struct {
-    Url    string    `json:"Url"`
-    Title  string    `json:"Title"`
-    Blocks []Blocker `json:"Blocks"`
+    ID    int    `db:"id"`
+    Uri   sql.NullString `db:"uri"`
+    Title string `db:"title"`
+	Blocks []Block
 }
 
-func getPageData(Url string) Page {
-    fmt.Println("getPageData: ", Url)
+type BlockData struct {
+    Collection string `db:"collection"`
+    ID         int    `db:"id"`
+    Item       string `db:"item"`
+    PageID     int    `db:"page_id"`
+    Sort       int    `db:"sort"`
+}
 
-    return Page{
-        Url:   "/",
-        Title: "Cookie's go-htmx - Home",
-    }
+type Block struct {
+	Collection string `db:"collection"`
+	Data map[string]interface{}
 }
 
 
-func getPageFromDBByUrl(pageUrl string) (Page, error) {
-	db, err := sqlx.Connect("postgres", "postgresql://directus@localhost:5432/directus")
+func getPageData(pageUrl string) (Page, error) {
+	db, err := sqlx.Connect("postgres", "user=directus dbname=directus password=Y25GUFMNeaGpEd sslmode=disable")
 
 	if err != nil {
 		return Page{}, err
@@ -88,45 +98,56 @@ func getPageFromDBByUrl(pageUrl string) (Page, error) {
 	defer db.Close()
 
 	var page Page
-	err = db.Get(&page, "SELECT id, url, title FROM pages WHERE url = $1", pageUrl)
+	if pageUrl == "/" {
+		err = db.Get(&page, "SELECT id, uri, title FROM page WHERE uri = '' OR uri IS NULL")
+	} else {
+		err = db.Get(&page, "SELECT id, uri, title FROM page WHERE uri = $1", pageUrl)
+	}
 	if err != nil {
 		return Page{}, err
 	}
 
-	// blocks, err := getBlocksFromDB(db, page.ID)
-	// if err != nil {
-	// 	return Page{}, err
-	// }
+	var blocksDatas []BlockData
+	err = db.Select(&blocksDatas, "SELECT collection, id, item, page_id, sort FROM page_blocks WHERE page_id = $1", page.ID)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("blocksDatas:", blocksDatas)
+	}
 
-	// page.Blocks = blocks
+	blocks := make([]Block, 0) // Initialize the Blocks map
+
+    for _, blockData := range blocksDatas {
+		block := Block{
+			Collection: blockData.Collection,
+			Data: make(map[string]interface{}),
+		} 
+		query := fmt.Sprintf("SELECT * FROM %s WHERE id = $1", blockData.Collection)
+		err = db.QueryRowx(query, blockData.ID).MapScan(block.Data)
+
+		if err != nil {
+            fmt.Println("Error querying for blocks:", err)
+            continue
+        }
+
+		// dynamically unmarshalling bytecode when a blocks field contains it (serialized json data usually)
+        for key, value := range block.Data {
+            if byteValue, ok := value.([]byte); ok {
+                var deserializedData interface{}
+                if err := json.Unmarshal(byteValue, &deserializedData); err == nil {
+                    block.Data[key] = deserializedData
+                } else {
+                    fmt.Println("Failed to unmarshal:", err)
+                }
+            }
+        }
+        
+        // Append to the overall list of blocks.
+        blocks = append(blocks, block)
+		// fmt.Println(blocks)
+	}
+
+	page.Blocks = blocks
+
 	return page, nil
 }
-
-
-
-
-// func getBlocksFromDB(pageID int) ([]map[string]interface{}, error) {
-// 	db, err := sqlx.Connect("postgres", "your_connection_string")
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	rows, err := db.Queryx("SELECT * FROM blocks WHERE page_id = $1", pageID)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer rows.Close()
-
-// 	var blocks []map[string]interface{}
-
-// 	for rows.Next() {
-// 		rowMap := make(map[string]interface{})
-// 		err := rows.MapScan(rowMap)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		blocks = append(blocks, rowMap)
-// 	}
-
-// 	return blocks, nil
-// }
