ee53750c6944fbe31f9e5d7f7b653a7fb18dabcf
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-11-12 20:46:07 -0800

    auto image optimization / generation on demand

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main
 go 1.20
 
 require (
+	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/evanw/esbuild v0.19.5 // indirect
 	github.com/fatih/color v1.16.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
@@ -10,5 +11,6 @@ require (
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/image v0.14.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,11 +10,18 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-	// "net/url"
+	"net/url"
+
 	"strings"
+	"image"
+
+	"crypto/sha1"
+    "io"
 
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+
+	"github.com/disintegration/imaging"
 )
 
 type Seo struct {
@@ -30,9 +37,6 @@ func main() {
 	versionHash := version
 
 	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("static"))
-	genFileServer := http.FileServer(http.Dir(".generated"))
 	
 	requestHandler := func(w http.ResponseWriter, r *http.Request) {
 		if (version == "development") {
@@ -110,11 +114,27 @@ func main() {
 		}
 	}
 
-	mux.Handle("/.generated/", maxAgeHandler(15552000, http.StripPrefix("/.generated/", genFileServer)))
+	// Handler for generated CSS files
+	cssFileServer := http.FileServer(http.Dir(".generated/css"))
+	mux.Handle("/css/", http.StripPrefix("/css/", cssFileServer))
+	
+	// Handler for image optimization
+	mux.Handle("/.generated/images/", http.HandlerFunc(imageHandler))
+
+	// Handler for static files like favicon, robots etc
+	fileServer := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/", maxAgeHandler(15552000, http.StripPrefix("/static/", fileServer)))
+
+	mux.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/robots.txt")
+	})
+	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "static/favicon.ico")
+	})
+
 	mux.HandleFunc("/", requestHandler)
 
-	log.Fatal(http.ListenAndServe(":42069", mux))
+	log.Fatal(http.ListenAndServe(":42069", mux))	
 }
 
 func maxAgeHandler(seconds int, h http.Handler) http.Handler {
@@ -261,59 +281,178 @@ func fileExists(filename string) bool {
     }
     return !info.IsDir()
 }
-
 func getImageProps(id string, options ...string) template.HTMLAttr {
-    validAttrs := map[string]bool{
-        "src": true, "width": true, "height": true, "class": true,
-        "alt": true, "title": true, // add other valid attributes here
-    }
     attrs := make(map[string]string)
-    attrs["src"] = "https://go-htmx-directus.cookieserver.gg/assets/" + id
-    var order []string
-    order = append(order, "src")
-
-    var layoutOption string // Variable to store the value of "layout" option
+    imageUrl := "https://go-htmx-directus.cookieserver.gg/assets/" + id
+    maxWidth := 1920 // Default maxWidth
+    customSrcsetProvided := false
+    customSizesProvided := false
+    lazyLoading := false // Default lazy loading
 
     for _, option := range options {
         parts := strings.Split(option, "=")
         if len(parts) == 2 {
             key := parts[0]
-            if validAttrs[key] {
-                if _, exists := attrs[key]; !exists {
-                    order = append(order, key) // Record the order of valid keys
+            value := parts[1]
+
+            switch key {
+            case "maxWidth":
+                var err error
+                maxWidth, err = strconv.Atoi(value)
+                if err != nil {
+                    maxWidth = 1920 // Reset to default if conversion fails
                 }
-                attrs[key] = parts[1]
-            } else {
-                // Process non-valid attributes here
-                if key == "layout" {
-                    layoutOption = parts[1] // Store the "layout" option for later use
-                    // Additional processing for "layout" can be done here
+            case "srcset":
+                customSrcsetProvided = true
+                attrs["srcset"] = value // Use the provided custom srcset
+            case "sizes":
+                customSizesProvided = true
+                attrs["sizes"] = value // Use the provided custom sizes
+            case "loading":
+                if value == "lazy" {
+                    lazyLoading = true
                 }
+            default:
+                attrs[key] = value // Handle other valid attributes
             }
         }
     }
 
-    // Handle class attribute
-    if classValue, exists := attrs["class"]; exists {
-        attrs["class"] = classValue + " image-wrapper"
-        if attrs["class"][0] == ' ' {
-            attrs["class"] = attrs["class"][1:]
+    // Generate default srcset if not provided
+    if !customSrcsetProvided {
+        widths := generateWidths(maxWidth)
+        srcsetValues := make([]string, len(widths))
+        for i, width := range widths {
+            optimizedImageUrl := fmt.Sprintf("/.generated/images?url=%s&width=%d", url.QueryEscape(imageUrl), width)
+            srcsetValues[i] = fmt.Sprintf("%s %dw", optimizedImageUrl, width)
         }
-    } else {
-        attrs["class"] = "image-wrapper"
-        order = append(order, "class")
+        attrs["srcset"] = strings.Join(srcsetValues, ", ")
+    }
+
+    // Generate default sizes if not provided
+    if !customSizesProvided {
+        attrs["sizes"] = "(max-width: 600px) 100vw, (max-width: 1024px) 50vw, 25vw"
+    }
+
+    attrs["src"] = fmt.Sprintf("/.generated/images?url=%s&width=%d", url.QueryEscape(imageUrl), maxWidth)
+    
+    if lazyLoading {
+        attrs["loading"] = "lazy"
     }
 
     var b strings.Builder
-    for _, key := range order {
-        b.WriteString(fmt.Sprintf(`%s="%s" `, key, attrs[key]))
+    for key, value := range attrs {
+        b.WriteString(fmt.Sprintf(`%s="%s" `, key, value))
     }
-
-    // Additional logic using layoutOption here, if needed
-	println(id)
-	println(layoutOption)
 
     return template.HTMLAttr(b.String())
 }
 
 
+
+// simple in-memory cache
+var optimizedImageCache = make(map[string]bool)
+func optimizeImage(url string, width int) (string, error) {
+    start := time.Now()
+    cacheKey := fmt.Sprintf("%s-%d", url, width)
+    
+    // Check cache first
+    if _, exists := optimizedImageCache[cacheKey]; exists {
+        optimizedImagePath := getOptimizedImagePath(url, width)
+		fmt.Println("image already processed: ", optimizedImagePath)
+        return optimizedImagePath, nil
+    }
+	fmt.Println("image not processed yet: ", url)
+
+    // Download the image from the URL
+    resp, err := http.Get(url)
+    if err != nil {
+        fmt.Println("failed to download image: ", err)
+        return "", err
+    }
+    defer resp.Body.Close()
+
+    // Decode the image
+    srcImage, _, err := image.Decode(resp.Body)
+    if err != nil {
+        fmt.Println("failed to decode image: ", err)
+        return "", err
+    }
+
+    // Image processing logic
+    dstImageFill := imaging.Fill(srcImage, width, width, imaging.Center, imaging.Lanczos)
+
+	imgPath := getOptimizedImagePath(url, width) // example .generated/images/3f6efd64ad786567e3ad5f558e01238bdb079ee3/500w.jpg
+	println(imgPath)
+
+	// make sure the directory exists (without the filename)
+	err = os.MkdirAll(imgPath[:strings.LastIndex(imgPath, "/")], 0755)
+	if err != nil {
+		fmt.Println("failed to create directory: ", err)
+		return "", err
+	}
+
+    // Save the file in .generated/images/<imageID>/<width>.jpg
+    err = imaging.Save(dstImageFill, imgPath)
+	
+
+    if err != nil {
+        fmt.Println("failed to save image: ", err)
+        return "", err
+    }
+
+    // Update the cache
+    optimizedImageCache[cacheKey] = true
+
+    // Log time duration
+    duration := time.Since(start)
+    fmt.Println("image processed: ", duration)
+
+    return getOptimizedImagePath(url, width), nil
+}
+
+
+func getOptimizedImagePath(url string, width int) string {
+	// encode url to be safe for urls and file systems
+	safeImageName := convertURLToFilePath(url)
+    return fmt.Sprintf(".generated/images/%s/%dw.jpg", safeImageName, width)
+}
+func convertURLToFilePath(url string) string {
+    h := sha1.New()
+    io.WriteString(h, url)
+    hashed := fmt.Sprintf("%x", h.Sum(nil))
+    return hashed
+}
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+    // Extract the necessary parameters from the URL
+    // For example, the image ID and the requested width
+    // This depends on how your URLs are structured
+    url := r.URL.Query().Get("url")
+    widthStr := r.URL.Query().Get("width")
+    width, err := strconv.Atoi(widthStr)
+    if err != nil {
+        http.Error(w, "Invalid width", http.StatusBadRequest)
+        return
+    }
+
+    optimizedImagePath, err := optimizeImage(url, width)
+    if err != nil {
+        // Handle errors (e.g., image not found, processing error)
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    // Set cache control headers
+    w.Header().Set("Cache-Control", "public, max-age=15552000")
+
+    // Serve the optimized image
+    http.ServeFile(w, r, optimizedImagePath)
+}
+
+func generateWidths(maxWidth int) []int {
+    // Define a set of widths up to maxWidth
+    // Example: [320, 480, 640, 800, 960, 1280, 1600, maxWidth]
+    // Implement logic to generate this array based on maxWidth
+	return []int{320, 480, 640, 768, 1024, 1280, 1600, maxWidth}
+}
